47b74add2ff661dcc1068d5c3613f5d24910b426
sip615 <sunilpatel.328@gmail.com>
2019-05-08 14:49:42 -0400

    first pass on the profile page component

---
+++ client/src/components/profile.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
+
+class ProfilePage extends Component {
+	render() {
+		return <div>hello</div>;
+	}
+}
+
+ProfilePage.propTypes = {};
+
+export default ProfilePage;
