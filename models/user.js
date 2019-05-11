0158709a66d50ccaf78c994acc2c8038755bceb2
Jennifer Gingras <jgingras@meca.edu>
2019-05-10 21:02:40 -0400

    added more columns to user table, added axios

--- models/user.js
+++ models/user.js
@@ -1,14 +1,39 @@
-module.exports = function(sequelize, DataTypes) {
-    var User = sequelize.define("User", {
-        email: DataTypes.STRING,
-        password: DataTypes.STRING
-    });
-
-    User.associate = function(models) {
-        User.hasMany(models.Stocks, {
-            onDelete: "cascade"
-        });
+module.exports = function (sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    goal: {
+      type: DataTypes.STRING,
     }
+  });
 
-    return User;
+  User.associate = function (models) {
+    User.hasMany(models.Stocks, {
+      onDelete: "cascade"
+    });
+  }
+
+  return User;
 }
