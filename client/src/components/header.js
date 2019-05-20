e78cd633dcfb4fce07f65000358842604a96c30e
sip615 <sunilpatel.328@gmail.com>
2019-05-20 18:59:38 -0400

    centered the title and changed it to Monkey Money

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Grid from "@material-ui/core/Grid";
 
 function Header() {
-  return (
-    <div className="header-bg">
-      <AppBar position="static" color="default">
-        <h2>Monkey Funds</h2>
-      </AppBar>
-    </div>
-  )
+	return (
+		<div className="header-bg">
+			<AppBar position="static" color="default">
+				<Grid container justify="center">
+					<h2>Monkey Money</h2>
+				</Grid>
+			</AppBar>
+		</div>
+	);
 }
 
 export default Header;
