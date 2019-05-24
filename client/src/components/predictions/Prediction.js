c0add2a4821c1677f80924d4ea22bc5d42701c71
ryan.w.daly <ryan.w.daly90@gmail.com>
2019-05-24 19:21:18 -0400

    add some temp jquery

--- client/src/components/predictions/Prediction.js
+++ client/src/components/predictions/Prediction.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link, BrowserRouter as Router } from 'react-router-dom';
 import "./Login.css";
 import { FormControl, InputLabel, Input } from '@material-ui/core/';
+import StockChart from "../stockChart"
 import axios from 'axios';
 import Header from '../header'
 import { conv2dDerFilter } from "@tensorflow/tfjs";
@@ -230,11 +231,103 @@ class Prediction extends Component {
 
         })
     }
+    $(document).ready(function () {
+
+        // Initialize the graph
+        plotData([], []);
+    
+        $('#getCompany').click(function () {
+            clearPrint();
+            print("Beginning Stock Prediction ...");
+            let company = $('#company').val().trim();
+            
+            $.getJSON(url.replace('%company%', company).toLowerCase()).then(function (data) {
+                
+                // Get the datetime labels use in graph
+                let labels = data.map(function (val) { return val['date']; });
+                
+                // Process the data and create the train sets
+                processData(data, timePortion).then(function (result) {
+                    
+                    // Crate the set for stock price prediction for the next day
+                    let nextDayPrediction = generateNextDayPrediction(result.originalData, result.timePortion);
+                    // Get the last date from the data set
+                    let predictDate = (new Date(labels[labels.length-1] + 'T00:00:00.000')).addDays(1);
+    
+                    // Build the Convolutional Tensorflow model
+                    buildCnn(result).then(function (built) {
+                        
+                        // Transform the data to tensor data
+                        // Reshape the data in neural network input format [number_of_samples, timePortion, 1];
+                        let tensorData = {
+                            tensorTrainX: tf.tensor1d(built.data.trainX).reshape([built.data.size, built.data.timePortion, 1]),
+                            tensorTrainY: tf.tensor1d(built.data.trainY)
+                        };
+                        // Rember the min and max in order to revert (min-max scaler) the scaled data later 
+                        let max = built.data.max;
+                        let min = built.data.min;
+                        
+                        // Train the model using the tensor data
+                        // Repeat multiple epochs so the error rate is smaller (better fit for the data)
+                        cnn(built.model, tensorData, epochs).then(function (model) {
+                            
+                            // Predict for the same train data
+                            // We gonna show the both (original, predicted) sets on the graph 
+                            // so we can see how well our model fits the data
+                            var predictedX = model.predict(tensorData.tensorTrainX);
+                            
+                            // Scale the next day features
+                            let nextDayPredictionScaled = minMaxScaler(nextDayPrediction, min, max);
+                            // Transform to tensor data
+                            let tensorNextDayPrediction = tf.tensor1d(nextDayPredictionScaled.data).reshape([1, built.data.timePortion, 1]);
+                            // Predict the next day stock price
+                            let predictedValue = model.predict(tensorNextDayPrediction);
+                            
+                            // Get the predicted data for the train set
+                            predictedValue.data().then(function (predValue) {
+                                // Revert the scaled features, so we get the real values
+                                let inversePredictedValue = minMaxInverseScaler(predValue, min, max);
+    
+                                // Get the next day predicted value
+                                predictedX.data().then(function (pred) {
+                                    // Revert the scaled feature
+                                    var predictedXInverse = minMaxInverseScaler(pred, min, max);
+    
+                                    // Convert Float32Array to regular Array, so we can add additional value
+                                    predictedXInverse.data = Array.prototype.slice.call(predictedXInverse.data);
+                                    // Add the next day predicted stock price so it's showed on the graph
+                                    predictedXInverse.data[predictedXInverse.data.length] = inversePredictedValue.data[0];
+        
+                                    // Revert the scaled labels from the trainY (original), 
+                                    // so we can compare them with the predicted one
+                                    var trainYInverse = minMaxInverseScaler(built.data.trainY, min, max);
+    
+                                    // Plot the original (trainY) and predicted values for the same features set (trainX)
+                                    plotData(trainYInverse.data, predictedXInverse.data, labels);
+                                });
+    
+                                // Print the predicted stock price value for the next day
+                                print("Predicted Stock Price of " + company + " for date " + moment(predictDate).format("DD-MM-YYYY") + " is: " + inversePredictedValue.data[0].toFixed(3) + "$");
+                            });
+                            
+                        });
+                        
+                    });
+                    
+                });
+                
+            });
+            
+            
+        });
+    });
 
     render() {
         return(
             <div>
-
+                <stockChart
+                    data={}
+                ></stockChart>
             </div>
         );
     }
